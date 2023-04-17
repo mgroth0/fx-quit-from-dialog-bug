a2b452bc0db3a1ad48c63b3598b07da7ecd2d1c4
mgroth0 <mgroth49@gmail.com>
2023-04-17 15:54:35 -0400

    remove other bug

--- src/main/kotlin/matt/fxquitfromdialogbug/HelloApplication.kt
+++ src/main/kotlin/matt/fxquitfromdialogbug/HelloApplication.kt
@@ -2,26 +2,17 @@ package matt.fxquitfromdialogbug
 
 import javafx.application.Application
 import javafx.scene.Scene
-import javafx.scene.control.Dialog
 import javafx.scene.input.KeyCode
 import javafx.scene.layout.VBox
 import javafx.stage.Modality
 import javafx.stage.Stage
 
-enum class Bug {
-    FrozenDialogOnQuit
-}
-
 class HelloApplication : Application() {
     override fun start(stage: Stage) {
         val scene = Scene(VBox(), 320.0, 240.0)
-        stage.title = "Hello!"
         stage.scene = scene.apply {
             setOnKeyPressed {
-                if (it.code == KeyCode.A && it.isMetaDown) {
-                    Dialog<String>().apply {
-                    }.showAndWait()
-                } else if (it.code == KeyCode.B && it.isMetaDown) {
+                if (it.code == KeyCode.B && it.isMetaDown) {
                     Stage().apply {
                         initModality(Modality.APPLICATION_MODAL)
                     }.showAndWait()
